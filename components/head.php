29dd303e7b018853bf3954cade09f3d0cca4aaef
Rafael Ponce <rafay.ponce@gmail.com>
2025-10-24 18:32:32 -0600

    Prueba despliegue automático 1

--- components/head.php
+++ components/head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<title>Sistema de Gestión Escolar UMG</title>
+<title>Sistema de Gestión Escolar TEST</title>
 <link rel="stylesheet" href="<?php echo BASE_STATIC; ?>assets/vendors/mdi/css/materialdesignicons.min.css?v=<?php echo mt_rand(); ?>" />
 <link rel="stylesheet" href="<?php echo BASE_STATIC; ?>assets/vendors/ti-icons/css/themify-icons.css?v=<?php echo mt_rand(); ?>" />
 <link rel="stylesheet" href="<?php echo BASE_STATIC; ?>assets/vendors/css/vendor.bundle.base.css?v=<?php echo mt_rand(); ?>" />
