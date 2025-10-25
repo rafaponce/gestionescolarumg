8e8a3035dca4847c48995dfd5aeff3ec65e4f9bd
Rafael Ponce <rafay.ponce@gmail.com>
2025-10-24 19:18:09 -0600

    Prueba-despliegue-automatico-3
    Prueba-despliegue-automatico 3

--- components/head.php
+++ components/head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<title>Sistema de Gestión Escolar UMG</title>
+<title>MASI - UMG</title>
 <link rel="stylesheet" href="<?php echo BASE_STATIC; ?>assets/vendors/mdi/css/materialdesignicons.min.css?v=<?php echo mt_rand(); ?>" />
 <link rel="stylesheet" href="<?php echo BASE_STATIC; ?>assets/vendors/ti-icons/css/themify-icons.css?v=<?php echo mt_rand(); ?>" />
 <link rel="stylesheet" href="<?php echo BASE_STATIC; ?>assets/vendors/css/vendor.bundle.base.css?v=<?php echo mt_rand(); ?>" />
